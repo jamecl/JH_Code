9b17812b2d10a08e42303842fba1966d598bcb64
Jim Clark <JimClark@Jims-MacBook-Pro.local>
2016-05-07 00:03:48 -0700

    updated SpeakGoodBye.js

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -18,7 +18,7 @@
 	// byeSpeaker object instead of being a standalone function.
 	// See Lecture 52, part 2
 	byeSpeaker.speak = function () {
-	  console.log(speakWord + " " + byeSpeaker.name());
+	  console.log(speakWord + " " + byeSpeaker.name);
 	}
 	window.byeSpeaker = byeSpeaker;
 
